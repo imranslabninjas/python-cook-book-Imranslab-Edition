f66d9a93f38319ca1e63a53dff6098bfd5669fb3
imranslabninjas <95885589+imranslabninjas@users.noreply.github.com>
2022-02-05 20:55:29 -0500

    Creating a Repository with a structure so that every one try and practice. It is all in public domain, Open Source code
    Signed-off-by: Islam, Imranul <imranul.islam@oracle.com>

---
+++ ClassWorks/Class 10/Chapter1.py
@@ -0,0 +1,9 @@
+# Chapter 1: Date Structures and Algorithms
+# Class 9
+# Instructor: Imranul Islam
+# Date February 06, 2022
+# (C) imranslab 2021-2022 under MIT License
+# The company is liable for any error
+
+
+# Class Exercise 1
