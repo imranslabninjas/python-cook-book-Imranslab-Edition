06c98671c8450baf26b4a37d32d917f9b1d65b15
Islam, Imranul <imranul.islam@oracle.com>
2022-02-05 21:43:44 -0500

    Updating some comments to make the code user-friendly
    Signed-off-by: Islam, Imranul <imranul.islam@oracle.com>

--- ClassWorks/Class 10/Chapter1.py
+++ ClassWorks/Class 10/Chapter1.py
@@ -19,6 +19,7 @@ print(x)  # Printing the value of x according to the structure
 print(y)  # Printing the value of y according to the structure
 
 data = ['ACME', 50, 91.1, (2022, 2, 5)]  # We are putting different data types in each bucket.
+
 # Look closely, you are storing String, int, float, some new kind of structure with a bunch of ints inside
 # Isn't it convenient to write in such format, Try in Java and Kotlin, it's different
 # Food for thought? how do we implement the same concept in Java, JavaScript? I am sure it is differnt? Challenge C?
@@ -29,3 +30,5 @@ print("name : " + str(name))  # Sneaking into the variable name
 print("shares : " + str(shares))  # Pikabo shares
 print("price : " + str(price))  # Hello price :p
 print("date : " + str(date))  # Hey date?!
+
+# If there is a mismatch in the number of elements, you will get an error. For example:
