6d49cf7acb8bbd3067993597f437074e668ac796
Islam, Imranul <imranul.islam@oracle.com>
2022-02-06 21:39:19 -0500

    File Reformat to make it look better with Python Docs
    Signed-off-by: Islam, Imranul <imranul.islam@oracle.com>

--- ClassWorks/Class 11/Chapter 1.py
+++ ClassWorks/Class 11/Chapter 1.py
@@ -1,11 +1,9 @@
-''' Unpacking Elements from Iterables of Arbitrary Length
-Today I learnt: the thriple ' thing can help make multiline comment output
-*** Problem: *** You need to unpack N elements from an iterable, but the iterable may be longer than N elements, causing a
-Solution: Python "Star expressions" can be used to address this problem. For example, suppose you run a course and decide
-at the end of the semester that you're going to drop the first and last homework grades, and only average the rest of them.
-If there are only four assignments, maybe you simply unpack all four, but what if there are 24? A star expression makes
-it easy:
-'''
+""" Unpacking Elements from Iterables of Arbitrary Length Today I learnt: the thriple ' thing can help make multiline
+comment output *** Problem: *** You need to unpack N elements from an iterable, but the iterable may be longer than N
+elements, causing a Solution: Python "Star expressions" can be used to address this problem. For example, suppose you
+run a course and decide at the end of the semester that you're going to drop the first and last homework grades,
+and only average the rest of them. If there are only four assignments, maybe you simply unpack all four, but what if
+there are 24? A star expression makes it easy: """
 from audioop import avg
 
 ''' 
@@ -30,15 +28,13 @@ print(name)
 print(email)
 print(*phone_numbers)
 
-'''
-It's worth nothing that the phone_numbers variable will always be a list, regardless of how many phone numbers are unpacked
-including none. Thus, any code that uses phone_numbers won't have to account for the possibility tht it might not be a list 
-or perform any kind of additional type checking.
+'''It's worth nothing that the phone_numbers variable will always be a list, regardless of how many phone numbers are 
+unpacked including none. Thus, any code that uses phone_numbers won't have to account for the possibility tht it 
+might not be a list or perform any kind of additional type checking. 
 
-The starred variable can also be the first one in the list. For example, say you have a sequence of values representng 
-your company's sales figures for the last eight quarters. If you want to see how the most recent quarter stacks up to the 
-average of the first seven, you could do something like this:
-'''
+The starred variable can also be the first one in the list. For example, say you have a sequence of values 
+representng your company's sales figures for the last eight quarters. If you want to see how the most recent quarter 
+stacks up to the average of the first seven, you could do something like this: '''
 
 sales_record = [1, 8, 9, 7, 6, 5, 4, 3, 2, 1]
 *trailing_qtrs, current_qtr = sales_record
