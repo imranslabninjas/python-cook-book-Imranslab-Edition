a175a8d7f21b73223af0f60bb144786ff6c1e8d4
SIyam Sorkar <rksiyam479.gmail.com>
2022-02-10 04:05:15 +0600

    Added an age calculator where you can calculate your current age.

--- StudentsWorks/SIyam Sorkar/SImple age calculator.py
+++ StudentsWorks/SIyam Sorkar/SImple age calculator.py
@@ -1,15 +1,27 @@
-# import datetime
-# now = datetime.datetime.now()
-# C_Date = now.strftime("%d-%m-%y")
-# C_Time = now.strftime("%H:%M:S")
-#
-# for date in C_Date:
-#     print(date)
-
-# print("Current date and time is:")
-# print(now.strftime("%y-%m-%d %H:%M:%S"))
+                        #The simple one to calculate only the year
 
 age = 2022
 birth_year = int(input("Enter your birth year: "))
 age -= (birth_year)     #Augmented assignment operator.
-print(f"Your current age is {age} years")   #Formatted string to simplify the code.
+print(f"Your are currently {age} years old")   #Formatted string to simplify the code.
+
+
+
+
+
+        #The perfect one for calculating exact year month and days. Note: It's not done yet
+
+import datetime
+# current_date  = int(datetime.datetime.now().strftime("%d"))
+# current_month = int(datetime.datetime.now().month)
+# current_year  = (datetime.datetime.year)
+
+# birth_date    = int(input("Enter your birth Date  here:    "))
+# birth_month   = int(input("Enter your birth month here:    "))
+# birth_year    = int(input("Enter your birth Year  here:    "))
+
+# age_date      = current_date  - birth_date
+# age_month     = current_month - birth_month
+# age_year      = 2022  - birth_year
+
+# print(f"Your current age is {age_year} year {age_month} month {age_date} days")
