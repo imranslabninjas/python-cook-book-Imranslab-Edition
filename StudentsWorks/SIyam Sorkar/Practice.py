afed21b44ffcb5787edffa31253d98a74154dcc8
SIyam Sorkar <rksiyam479.gmail.com>
2022-02-10 01:56:50 +0600

    Added Practice.py to Practice my daily lessons

--- StudentsWorks/SIyam Sorkar/Practice.py
+++ StudentsWorks/SIyam Sorkar/Practice.py
@@ -1,15 +1,5 @@
-# import datetime
-# now = datetime.datetime.now()
-# C_Date = now.strftime("%d-%m-%y")
-# C_Time = now.strftime("%H:%M:S")
-#
-# for date in C_Date:
-#     print(date)
-
-# print("Current date and time is:")
-# print(now.strftime("%y-%m-%d %H:%M:%S"))
-
-age = 2022
-birth_year = int(input("Enter your birth year: "))
-age -= (birth_year)     #Augmented assignment operator.
-print(f"Your current age is {age} years")
+# numbers = [5, 2, 5, 2, 2]
+# for F in numbers:
+#     for number in F:
+#         style =  * number
+#         print(style)
