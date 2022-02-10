1ff86f12de8870c1e09fbecec9243c61762eab9b
Sahadab <sahadabshishir135@gmail.com>
2022-02-10 21:49:34 +0600

    practice

--- StudentsWorks/Sahadab islam/mine.py
+++ StudentsWorks/Sahadab islam/mine.py
@@ -1,7 +1,27 @@
-record = ('ACME', 50, 123.45, (12, 18, 19, 2012))
-name, *_, (*__, year) = record
+# record = ('ACME', 50, 123.45, (12, 18, 19, 2012))
+# name, *_, (*__, year) = record
+#
+# print(name)
+# print(*_)
+# print(*__)
+# print(year)
 
-print(name)
-print(*_)
-print(*__)
-print(year)
+record = \
+    [
+        ("jam", 1, 3.01),
+        ("jelly", 1, 2),
+        ("jam", 2, 7.43),
+    ]
+
+def do_foo(x, y):
+    print('jam', x, y)
+
+
+def do_bar(s, a):
+    print('jelly', s)
+
+for tag, *args in record:
+    if tag == "jam":
+        do_foo(*args)
+    elif tag == "jelly":
+        do_bar(*args)
