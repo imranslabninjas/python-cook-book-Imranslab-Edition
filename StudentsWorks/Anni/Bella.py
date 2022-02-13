4cb23ddf0823b08cb0001ba96efef34a136f3b86
Anni <shoptorsijahan@gmail.com>
2022-02-13 18:16:01 +0600

    Unpacking a Sequence into Separate Variables

--- StudentsWorks/Anni/Bella.py
+++ StudentsWorks/Anni/Bella.py
@@ -1,1 +1,16 @@
-print("Anni")
+# data= ['pran',20,10, (2022,12,13)]
+#
+# name,age,price,date = data
+# print(name)
+# print(price)
+# print(age)
+# print(date)
+
+name='bella'
+a,b,c,d,e=name
+print(a,b,c,d,e)
+print(a)
+print(b)
+print(c)
+print(b)
+print(e)
