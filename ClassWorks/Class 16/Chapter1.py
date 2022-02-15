063c958a0584ebeea4d2e796d060e8a25d39c4e3
annyhira414 <annyhira@gmail.com>
2022-02-16 00:13:47 +0600

    Practice on Assign Multiple Values  from w3 school

--- ClassWorks/Class 16/Chapter1.py
+++ ClassWorks/Class 16/Chapter1.py
@@ -1,37 +1,54 @@
-import heapq
+# # import heapq
+# #
+# # numbers = [1, 2, 4, 0, 2, 6, 17, 19, 25, -1, -2, -3, 41, 42, 43, 55, 65, 75, 85]
+# #
+# # print(heapq.nlargest(10, numbers))
+# # print(heapq.nsmallest(10, numbers))
+#
+# # '''
+# # Lets use some more weird data and more complicated data to sort
+# # '''
+# portfolio = [
+#     {'name': 'IBM', 'shares': 100, 'price': 10},
+#     {'name': 'FB', 'shares': 80, 'price': 20},
+#     {'name': 'Intel', 'shares': 90, 'price': 5},
+#     {'name': 'Qualcomm', 'shares': 110, 'price': 70},
+#     {'name': 'AMD', 'shares': 120, 'price': 40},
+#     {'name': 'Microsoft', 'shares': 10, 'price': 60},
+#     {'name': 'Oracle', 'shares': 200, 'price': 100},
+# ]
+# # '''
+# # Cheapest and most expensive shares from a complicated data structure
+# # '''
+# # cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+# # expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+# #
+# # print(cheap)
+# # print(expensive)
+# #
+# # '''
+# # Minimum and maximum number of shares from a complicated data structure
+# # '''
+# #
+# # minNumberShares = heapq.nsmallest(3, portfolio, key=lambda s: s['shares'])
+# # maxNumberShares = heapq.nlargest(3, portfolio, key=lambda s: s['shares'])
+# #
+# # print(minNumberShares)
+# # print(maxNumberShares)
 
-numbers = [1, 2, 4, 0, 2, 6, 17, 19, 25, -1, -2, -3, 41, 42, 43, 55, 65, 75, 85]
 
-print(heapq.nlargest(3, numbers))
-print(heapq.nsmallest(3, numbers))
+A = list(range(1, 10))
+B = list(range(10))
+C = list(range(10))
+M = []
+for a in A:
+    for b in B:
+        for c in C:
+          N= 100*a+10*b+c
+if (N % 3) == 2:
+  M.append(N)
+W = M[0]
+for i in range(2, len(M)+1):
+  W = W+M[i+1]
+print(W)
 
-'''
-Lets use some more weird data and more complicated data to sort
-'''
-portfolio = [
-    {'name': 'IBM', 'shares': 100, 'price': 10},
-    {'name': 'FB', 'shares': 80, 'price': 20},
-    {'name': 'Intel', 'shares': 90, 'price': 5},
-    {'name': 'Qualcomm', 'shares': 110, 'price': 70},
-    {'name': 'AMD', 'shares': 120, 'price': 40},
-    {'name': 'Microsoft', 'shares': 10, 'price': 60},
-    {'name': 'Oracle', 'shares': 200, 'price': 100},
-]
-'''
-Cheapest and most expensive shares from a complicated data structure
-'''
-cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
-expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
-
-print(cheap)
-print(expensive)
-
-'''
-Minimum and maximum number of shares from a complicated data structure
-'''
-
-minNumberShares = heapq.nsmallest(3, portfolio, key=lambda s: s['shares'])
-maxNumberShares = heapq.nlargest(3, portfolio, key=lambda s: s['shares'])
-
-print(minNumberShares)
-print(maxNumberShares)
--- StudentsWorks/Hira/w3 school/work.py
+++ StudentsWorks/Hira/w3 school/work.py
@@ -60,7 +60,37 @@ if 5 > 2:
  print("Five is greater than two!")
       print("Five is greater than two!")
 '''
-
 if 5 > 2:
     print("Five is greater than two! ")
     print("and this is a right way")
+
+
+'''
+https://www.w3schools.com/python/python_variables_multiple.asp
+Date : 15/2/2020a~!
+
+Python Variables - Assign Multiple Values
+Many Values to Multiple Variables
+Python allows you to assign values to multiple variables in one line:
+
+
+'''
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#One Value to Multiple Variables
+
+x = y = z = "apply"
+print(x)
+print(y)
+print(z)
+
+'''
+Unpack a Collection
+If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+'''
+
+fruits = ['banana', 'apple', 'orange']
+x ,y ,z =fruits
+print(x)
