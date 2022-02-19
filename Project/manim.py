d21f4c4dd975fc9a141c2e25e5c7da8bcb09e886
Islam, Imranul <imranul.islam@oracle.com>
2022-02-18 20:52:51 -0500

    Just scribbing down to transfer to the pc
    Signed-off-by: Islam, Imranul <imranul.islam@oracle.com>

---
+++ Project/manim.py
@@ -0,0 +1,18 @@
+from manim import *
+
+
+class MyFirstAnimation(Scene):
+    def construct(self):
+        # Create basic mobjects
+        star = Star(n=5, fill_color=RED, stroke_color=BLUE)
+        circle = Circle(fill_color=DARK_BLUE, fill_opacity=0.8, stroke_color=BLUE)
+
+        # Animate Fade in of the star that takes 2 seconds
+        self.play(FadeIn(star, run_time=2))
+        # Wait for a second
+        self.wait()
+        # Transform star into a circle
+        self.play(Transform(star, circle))
+        self.wait(0.5)
+        # Remove the circle by fading it out
+        self.play(FadeOut(circle))
