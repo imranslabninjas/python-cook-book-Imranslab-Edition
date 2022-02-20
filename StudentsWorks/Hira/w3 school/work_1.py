c01a537a353e4c40cc4744f7715b42bb12be2999
annyhira414 <annyhira@gmail.com>
2022-02-20 23:08:48 +0600

    Practice on python number ,random number in python  , python casting , python string- Multiline Strings and Looping Through a String from W3 school

--- StudentsWorks/Hira/w3 school/work_1.py
+++ StudentsWorks/Hira/w3 school/work_1.py
@@ -23,6 +23,7 @@ Binary Types:	bytes, bytearray, memoryview
 Getting the Data Type
 You can get the data type of any object by using the type() function:
 '''
+
 x = "Hira"
 print(type(x))
 
@@ -82,3 +83,76 @@ x6 = range(6)
 x7 = dict(name="John", age=36)
 x8 = set(("apple", "chips", "cherry"))
 x9 = frozenset(("apple", "banana", "cherry"))
+
+
+
+'''
+https://www.w3schools.com/python/python_numbers.asp
+Date  : 20/ 2 /2022
+
+'''
+
+'''
+Random Number
+Python does  "not" have a random() function to make a random number, 
+but Python has a "built-in module" called random that can be used to make random numbers:
+
+Import the random module, and display a random number between 1 and 9:
+'''
+import random
+print("Here is show a random number between 1 to 10 : ", random.randrange(1, 10))
+
+
+'''
+https://www.w3schools.com/python/python_casting.asp
+ python casting -  There may be times when you want to specify a type on to a variable.
+ This can be done with casting.
+ its works for Specify a Variable Type
+ int()
+ float()
+ srt()
+ 
+'''
+
+"""
+Python Strings
+
+"""
+myName = 'hira'
+print(myName)
+'''
+Multiline Strings
+You can assign a multiline string to a variable by using ''''''three'''''' quotes:
+'''
+text = ''' 
+I am Hira ,
+I love coffee,
+I love my family ,
+I have a pet cat '''
+print(text)
+
+
+'''
+Strings are Arrays
+Like many other popular programming languages, 
+strings in Python are arrays of bytes representing unicode characters.
+
+However, Python does not have a character data type, a single character is simply a string with a length of 1.
+
+Square brackets can be used to access elements of the string.
+
+every element can be a cut and it can be show the user 
+'''
+a = "Hello, World!"
+print(a[0])
+print(a[1])
+print(a[0])
+print(a[1])
+print(a[0])
+print(a[1])
+
+print("Looping Through a String")
+for x in 'myname':
+    print(x)
+
+
