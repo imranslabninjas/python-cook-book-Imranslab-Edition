3bcd68ec06c28da4b24b1d6a9bbbdf9baf1e5842
annyhira414 <annyhira@gmail.com>
2022-03-03 15:18:27 +0600

    practice lambda function

---
+++ StudentsWorks/Hira/w3 school/lambda.py
@@ -0,0 +1,16 @@
+''''
+3/3/2022
+Python Lambda
+A lambda function is a small anonymous function.
+
+A lambda function can take any number of arguments, but can only have one expression.
+
+'''
+
+x = lambda a: a + 4
+print(x(2))  # a= 2
+
+numbers = lambda a, v, b, e, r, k: a + v + b + e + r + k
+print(numbers(1,2,3,4,5,3))
+
+
