1bbc34871bc3df9a3d9596b7856aeaa6e67c6ce4
annyhira414 <annyhira@gmail.com>
2022-03-04 17:02:16 +0600

    practice lambda function

--- StudentsWorks/Hira/w3 school/lambda.py
+++ StudentsWorks/Hira/w3 school/lambda.py
@@ -6,11 +6,23 @@ A lambda function is a small anonymous function.
 A lambda function can take any number of arguments, but can only have one expression.
 
 '''
+#
+# b = lambda a: a + 4
+# print(b(2))  # a= 2
+#
+# numbers = lambda a, v, b1, e, r, k: a + v + b1 + e + r + k
+# print(numbers(1, 2, 3, 4, 5, 3))
 
-x = lambda a: a + 4
-print(x(2))  # a= 2
+'''
+1. Write a Python program to create a lambda function that adds 15 to a given number
+ passed in as an argument, also create a lambda function
+ that multiplies argument x with argument y and print the result
+'''
+a = lambda b: b + 15
 
-numbers = lambda a, v, b, e, r, k: a + v + b + e + r + k
-print(numbers(1,2,3,4,5,3))
+print("1st part : ", a(10))
+
+num = lambda x, y: x * y
+print("2nd part : ", num(12, 4))
 
 
