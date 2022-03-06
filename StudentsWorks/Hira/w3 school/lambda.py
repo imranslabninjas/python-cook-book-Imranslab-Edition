a21d3f0a046ed27fc1567563739a5f79a985aea7
annyhira414 <annyhira@gmail.com>
2022-03-06 23:10:27 +0600

    practice lambda function problem

--- StudentsWorks/Hira/w3 school/lambda.py
+++ StudentsWorks/Hira/w3 school/lambda.py
@@ -25,4 +25,32 @@ print("1st part : ", a(10))
 num = lambda x, y: x * y
 print("2nd part : ", num(12, 4))
 
+'''
+Date: 3/6/2022
+Write a Python program to create a function that takes one argument,
+and that argument will be multiplied with an unknown given number.
+
+'''
+
+
+def func_compute(n):
+    return lambda x: x * n
+
+
+result = func_compute(2)
+print("Double the number of 15 =", result(15))
+result = func_compute(3)
+print("Triple the number of 15 =", result(15))
+result = func_compute(4)
+print("Quadruple the number of 15 =", result(15))
+result = func_compute(5)
+print("Quintuple the number 15 =", result(15))
+
+
+def counter_number(n):
+    return lambda x: x * n  # new Things is learned  lambda function can be return without passing a variable mane
+
+
+new_result = counter_number(2)  # n = 2
+print(new_result(3))  # here is x = 3
 
