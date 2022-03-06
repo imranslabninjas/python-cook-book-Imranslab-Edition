a6c74039d93edaade454133d492e149cd9de88a5
annyhira414 <annyhira@gmail.com>
2022-03-06 23:46:56 +0600

    practice lambda function problem 4

--- StudentsWorks/Hira/w3 school/lambda.py
+++ StudentsWorks/Hira/w3 school/lambda.py
@@ -68,3 +68,15 @@ print("Original list of tuples:", subject_marks)
 
 subject_marks.sort(key=lambda x: x[1])
 print(subject_marks)
+
+'''
+4 Write a Python program to sort a list of dictionaries using Lambda.
+
+
+'''
+models = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'},
+          {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+print("Original list of dictionaries :", models)
+
+new_models = sorted(models, key=lambda x: x['color'])
+print(new_models)
