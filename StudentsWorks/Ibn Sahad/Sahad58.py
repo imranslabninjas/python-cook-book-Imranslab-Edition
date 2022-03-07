0f528c0b9077d9db4feb185039982a821afb2ab4
Ibn Sahad <ibnshahad053@gmail.com>
2022-03-07 23:47:41 +0600

    Clook project

--- StudentsWorks/Ibn Sahad/Sahad58.py
+++ StudentsWorks/Ibn Sahad/Sahad58.py
@@ -1,3 +1,17 @@
-print("Hello World")
+from tkinter import *
+from tkinter.ttk import *
 
+from time import strftime
 
+root = tk()
+root.title("clook")
+def time():
+    string  = strftime("%H,%M,%S")
+    label.config(text=string)
+    label.after(1000, time)
+
+label = label(root, font=("ds-digital, 80") background="black", foreground="cyan")
+label.pack(anchor="center")
+time()
+
+mainloop()
