0420712121e9d9a0fa6ce436d8df49dfd91d439f
annyhira414 <annyhira@gmail.com>
2022-03-11 00:06:52 +0600

    problem practice check the string or numeric

--- StudentsWorks/Hira/Problem/geeksforgeeks.org/Given a string, write a Python program to check if the string is numeric or not..py
+++ StudentsWorks/Hira/Problem/geeksforgeeks.org/Given a string, write a Python program to check if the string is numeric or not..py
@@ -1,52 +1,52 @@
-# Python program to identify the Digit
+# # Python program to identify the Digit
+#
+# # import re module
+#
+# # re module provides support
+# # for regular expressions
+# import re
+#
+# # Make a regular expression
+# # for identifying a digit
+# regex = '^[0-9]+$'
+#
+#
+# # Define a function for
+# # identifying a Digit
+# def check(string):
+#     # pass the regular expression
+#     # and the string in search() method
+#     if (re.search(regex, string)):
+#         print("Digit")
+#
+#     else:
+#         print("Not a Digit")
+#
+#
+# # Driver Code
+# if __name__ == '__main__':
+#     # Enter the string
+#     string = "28"
+#
+#     # calling run function
+#     check(string)
+#
+#     string = "a"
+#     check(string)
+#
+#     string = "21ab"
+#     check(string)
+#
+#     string = "12ab12"
+#     check(string)
 
-# import re module
-
-# re module provides support
-# for regular expressions
-import re
-
-# Make a regular expression
-# for identifying a digit
-regex = '^[0-9]+$'
-
-
-# Define a function for
-# identifying a Digit
-def check(string):
-    # pass the regular expression
-    # and the string in search() method
-    if (re.search(regex, string)):
-        print("Digit")
-
-    else:
-        print("Not a Digit")
-
-
-# Driver Code
-if __name__ == '__main__':
-    # Enter the string
-    string = "28"
-
-    # calling run function
-    check(string)
-
-    string = "a"
-    check(string)
-
-    string = "21ab"
-    check(string)
-
-    string = "12ab12"
-    check(string)
-
-    '''
+'''
     my practice 
     '''
 
-    import re
+import re
 
-    dontuse = '^[0-9]+$'
+dontuse = '^[0-9]+$'
 
 
 def checking(string):
@@ -56,10 +56,21 @@ def checking(string):
         print("not valid")
 
 
-
-if __main__ =  "__main__":
+if __name__ == "__main__":
     # Enter the string
     string = "28"
 
     # calling run function
-    check(string)
+    checking(string)
+
+    string = "k"
+    checking(string)
+
+    string = "a"
+    checking(string)
+
+    string = "21ab"
+    checking(string)
+
+    string = "12ab12"
+    checking(string)
