31141e6c75f1352c6f3d4a9672229218f5d7bfde
annyhira414 <annyhira@gmail.com>
2022-03-11 23:57:39 +0600

    to count the even, odd numbers

--- StudentsWorks/Hira/Problem/Lambda/lambda 11-15.py
+++ StudentsWorks/Hira/Problem/Lambda/lambda 11-15.py
@@ -22,3 +22,16 @@ print(array_nums)
 result = sorted(array_nums, key=lambda i: 0 if i == 0 else -1 / i)
 print("\nRearrange positive and negative numbers of the said array:")
 print(result)
+
+'''
+13. Write a Python program to count the even, odd numbers in a given array of integers using Lambda
+that means fine the lenth
+'''
+
+array_nums = [1, 2, 3, 5, 7, 8, 9, 10]
+print("Original arrays:")
+print(array_nums)
+odd_ctr = len(list(filter(lambda x: (x % 2 != 0), array_nums)))
+even_ctr = len(list(filter(lambda x: (x % 2 == 0), array_nums)))
+print("\nNumber of even numbers in the above array: ", even_ctr)
+print("\nNumber of odd numbers in the above array: ", odd_ctr)
