768a1d8e432b1cc1806037bb6bc326a96b7e8eed
annyhira414 <annyhira@gmail.com>
2022-03-12 23:26:10 +0600

    practice lambda function

--- StudentsWorks/Hira/Problem/Lambda/lambda 11-15.py
+++ StudentsWorks/Hira/Problem/Lambda/lambda 11-15.py
@@ -13,6 +13,21 @@ print(array_nums2)
 result = list(filter(lambda x: x in array_nums1, array_nums2))
 print("\nIntersection of the said arrays: ", result)
 
+# me
+s_Id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9]
+s_rool = [2, 3, 4, 5, 6, 7, 11]
+
+result = list(filter(lambda x: x in s_Id, s_rool))
+print('\nthe common value', result)
+
+# m2
+
+num1_list = [22, 33, 33, 44, 55, 11, 66, 66, 77, 777, 888, 888]
+num2_list = [22, 33, 44, 55, 66, 77, 78, 78, 77, 899, 90]
+print('the main list: ', num2_list, num1_list)
+list_re = list(filter(lambda v: v in num2_list, num1_list))
+print("Hera is the common value:", list_re)
+
 '''
 12. Rearrange positive and negative numbers in a given array using Lambda
 '''
@@ -29,12 +44,11 @@ that means fine the lenth
 '''
 
 array_nums = [1, 2, 3, 5, 7, 8, 9, 10]
-print("Original arrays:")
 print(array_nums)
 odd_ctr = len(list(filter(lambda x: (x % 2 != 0), array_nums)))
 even_ctr = len(list(filter(lambda x: (x % 2 == 0), array_nums)))
-print("\nNumber of even numbers in the above array: ", even_ctr)
-print("\nNumber of odd numbers in the above array: ", odd_ctr)
+print("\nNumber of even numbers : ", even_ctr)
+print("\nNumber of odd numbers : ", odd_ctr)
 
 '''
 14 .Write a Python program to find the values of length six in a given list using Lambda.
@@ -45,4 +59,4 @@ print("\n")
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 days = filter(lambda day: day if len(day) == 6 else '', weekdays)
 for d in days:
-    print("wow 3 days ",d)
+    print("wow 3 days ", d)
