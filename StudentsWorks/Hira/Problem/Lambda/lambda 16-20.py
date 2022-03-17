4444e936180eff6d6cc42f7032023a50b100b331
annyhira414 <annyhira@gmail.com>
2022-03-17 22:08:02 +0600

    find palindromes

--- StudentsWorks/Hira/Problem/Lambda/lambda 16-20.py
+++ StudentsWorks/Hira/Problem/Lambda/lambda 16-20.py
@@ -19,3 +19,18 @@ sec_student_name.sort()
 print("\nNames:")
 for s_name in sec_student_name:
     print(s_name)
+
+# 17 .Write a Python program to find palindromes in a given list of strings using Lambda.
+texts = ["php", "w3r", "Python", "abcd", "Java", "aaa"]
+print("Orginal list of strings:")
+print(texts)
+result = list(filter(lambda x: (x == "".join(reversed(x))), texts))
+print("\nList of palindromes:")
+print(result)
+
+txt =['hih', "aba", "cg"]
+#number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 202]
+print(txt)
+found = list(filter(lambda x: (x == "".join(reversed(x))), txt))
+print(found)
+
