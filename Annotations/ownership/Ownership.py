73d5d11f9308c3fad0c71461492aec6cbe44eafa
Islam, Imranul <imranul.islam@oracle.com>
2022-03-27 12:05:03 -0400

    Just created some example, not sure if it is right yet, more work to do here

---
+++ Annotations/ownership/Ownership.py
@@ -0,0 +1,10 @@
+
+class Ownership:
+    def __init__(self, staff, student):
+        self.staff = staff
+        self.student = student
+
+    def setStaff(self, staff):
+        self.staff = staff
+
+
---
+++ Annotations/ownership/staff/staff.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Staff(Enum):
+    farzanasupti = "Farzana Supti"
+    sazzadbhuiyan = "Sazzad Bhuiyan"
+    woalidhassan = "Woalid Hassan"
+    annyhira = "Anny Hira"
+    masumkazi = "Masum Kazi"
+    ashiqurrahman = "Md Ashiqur Rahman"
+    mahammadrasel = "Mohammad Rasel"
