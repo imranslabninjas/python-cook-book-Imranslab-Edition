a9dd5297fadc2ef9cc8c5ab8972c229d26e95118
Islam, Imranul <imranul.islam@oracle.com>
2022-03-30 13:00:09 -0400

    Anagrams interview question more solutions

--- InterviewQuestionsSolved/anagrams.py
+++ InterviewQuestionsSolved/anagrams.py
@@ -26,7 +26,7 @@ def solution(string1, string2):
             character_frequency_dictionary[letter] = 1
 
     for letter in string2:
-        if letter in character_frequency_dictionary:
+        if letter in character_frequency_dictionary and character_frequency_dictionary[letter] > 0:
             character_frequency_dictionary[letter] -= 1
         else:
             return False
@@ -36,8 +36,36 @@ def solution(string1, string2):
     else:
         return False
 
+"""Assuming we can import Counter from collections"""
+from collections import Counter
+def solution2(string1, string2):
+    if len(string1) != len(string2):
+        return False
+
+    else:
+        string1 = sorted(string1.lower())
+        string2 = sorted(string2.lower())
+
+        return Counter(string1) == Counter(string2)
+
+
+"""Using Sorting function
+T(n) = O(nlogn) + o(nlogn) + n
+    ~= n
+"""
+def solution3(string1, string2):
+    if len(string1) != len(string2):
+        return False
+
+    string1 = string1.lower()
+    string2 = string2.lower()
+
+    return sorted(string1) == sorted(string2)
+
 
 """Driver Code"""
-s1 = "Race"
-s2 = "Care"
+s1 = "nameless"
+s2 = "salesman"
 print("The String is :" + str(solution(s1, s2)))
+print("The String is :" + str(solution2(s1, s2)))
+print("The String is :" + str(solution3(s1, s2)))
