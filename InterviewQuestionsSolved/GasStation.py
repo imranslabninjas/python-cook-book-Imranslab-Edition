59a8760e97ca960deef2ffe035c0473cda9c421c
Islam, Imranul <imranul.islam@oracle.com>
2022-04-02 12:41:24 -0400

    Gas Station problem statement added

---
+++ InterviewQuestionsSolved/GasStation.py
@@ -0,0 +1,10 @@
+"""
+Note that:
+- We can only move forward
+- The gas tank starts empty
+- gas[i] represents the amount of gas at the station i
+- cost[i] represents the cost to go from the station i to the next one
+- the answer is guaranteed to be unique
+- if the station we'are searching for doesn't exist, return -1
+"""
+
