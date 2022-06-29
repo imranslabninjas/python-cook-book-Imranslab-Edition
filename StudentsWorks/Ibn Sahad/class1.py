5f1d13e6b2f88fc065fe34698bbb20324963529c
Ibn Sahad <ibnshahad053@gmail.com>
2022-06-30 00:26:02 +0600

    1st  up

--- StudentsWorks/Ibn Sahad/class1.py
+++ StudentsWorks/Ibn Sahad/class1.py
@@ -3,4 +3,24 @@ Name = 'Ibn Sahad'
 print(Name)
 main = 1+8+5
 print(main)
-
+x= 5
+y= "Sahad"
+print(x)
+print(y)
+x= 4        #x is of type int
+x= "Sally"  #x is now type of srt
+print(x)
+x= str(3)
+y= int(3)
+z= float(3)
+print(x)
+print(y)
+print(z)
+x= 4
+y="Imranslab"
+print(type(x))
+print(type(y))
+u= 565
+U= "Ibn"
+print(u)
+print(U)
