98253df9b03580a7060d4ebd732688a6a818b61a
Busra <789busra@gmail.com>
2022-06-30 19:32:44 +0600

    if else

--- StudentsWorks/Busra/test.py
+++ StudentsWorks/Busra/test.py
@@ -1,3 +1,5 @@
+import keyword
+print(keyword.kwlist)
 print("Hi")
 
 x = "Python, is, great, right?"
@@ -13,3 +15,33 @@ print(box1)
 number = [2,4,5,6,9]
 a,b,c,d,e = number
 print(a)
+
+"""
+Hi
+"""
+x = 1
+print("x=1")
+
+x=1
+x=2
+print(x)
+
+y = 2
+y **=2
+print(y)
+
+number = 8
+
+if number<4:
+    print("positive")
+else:
+    print("neg")
+
+print(f"{number} is true")
+
+answer = int(input('What is the answer to 1+2?'))
+def equation(answer):
+    if answer == 3:
+        print ('Correct!')
+    else:
+        print ('Wrong, it is 3!')
