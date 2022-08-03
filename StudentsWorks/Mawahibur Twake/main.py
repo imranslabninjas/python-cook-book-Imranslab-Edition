c01db6b30ba3f06a79b2c31e8ed95a78aaed1d29
Thoki135 <64465033+Thoki135@users.noreply.github.com>
2022-08-03 07:35:23 +0600

    Add files via upload

---
+++ StudentsWorks/Mawahibur Twake/main.py
@@ -0,0 +1,5 @@
+print('Hello, World!')
+print(1+2)
+print(7*8)
+print()
+print("The End")
---
+++ StudentsWorks/Mawahibur Twake/repfields.py
@@ -0,0 +1,14 @@
+age = 24
+print("My age is " + str(age) +" years")
+
+print("""Jan: {2}
+Feb: {0}
+Mar: {2}
+Apr: {1}
+May: {2}
+jun: {1}
+Jul: {2}
+Sep: {1}
+Oct: {2}
+Nov: {1}
+Dec: {2}""".format(28,30,31))
