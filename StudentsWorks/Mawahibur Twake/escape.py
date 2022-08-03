725541a18dd5518f84c77f65be9c5298218e4ec7
Thoki135 <64465033+Thoki135@users.noreply.github.com>
2022-08-03 07:31:24 +0600

    Python
    Stepping into the world Python

---
+++ StudentsWorks/Mawahibur Twake/escape.py
@@ -0,0 +1,5 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString ="1\t2\t3"
+print(tabbedString)
---
+++ StudentsWorks/Mawahibur Twake/sliceback.py
@@ -0,0 +1,13 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards  = letters[25:0:-1]
+print(backwards)
+
+
+print(letters[16:13:-1])
+
+
+print(letters[4::-1])
+
+
+print(letters[:-9:-1])
